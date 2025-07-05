e07ea7ebdd393942e223e495c9b66e108f34f4f3
jistro <contact@jistro.xyz>
2025-07-05 23:49:44 +0200

    Refactor PaymentPage to include payment functionality and enhance receiver information inputs

--- config/index.tsx
+++ config/index.tsx
@@ -1,15 +1,17 @@
-import { cookieStorage, createStorage, http } from '@wagmi/core'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet, arbitrum } from '@reown/appkit/networks'
+import { cookieStorage, createStorage } from "wagmi";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { mainnet, arbitrumSepolia } from '@reown/appkit/networks'
+import type { AppKitNetwork } from "@reown/appkit/networks";
 
 // Get projectId from https://cloud.reown.com
-export const projectId = "5cf8016487f0a0fdf4b687f98346e8c5"
+export const projectId =
+  process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694"; // this is a public projectId only to use on localhost
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
-export const networks = [mainnet, arbitrum]
+export const networks = [arbitrumSepolia]
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
